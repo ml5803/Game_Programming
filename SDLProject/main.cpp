e83f1bc28dd00703d82555c9dab014806c256450
Michael Li <ml5803@nyu.edu>
2020-02-16 14:19:18 -0500

    project 1 added mario, fireball and mushroom

--- SDLProject/main.cpp
+++ SDLProject/main.cpp
@@ -18,12 +18,17 @@ SDL_Window* displayWindow;
 bool gameIsRunning = true;
 
 ShaderProgram program;
-glm::mat4 viewMatrix, modelMatrix, projectionMatrix;
+glm::mat4 viewMatrix, modelMatrix, modelMatrix2, modelMatrix3, marioMatrix, projectionMatrix;
 
-GLuint fireballTextureID;
+GLuint fireballTextureID, fireballTextureID2, marioTextureID, mushroomTextureID;
 
-float ball_x = 0;
+float ball_x = -5;
 float ball_rotate = 0;
+
+float mario_y = 0;
+bool mario_up = true;
+
+float mush_x = -5;
 
 GLuint LoadTexture(const char* filepath){
     int w, h, n;
@@ -56,17 +61,18 @@ void Initialize() {
     glewInit();
 #endif
     
-    glViewport(0, 0, 640, 480);
+    glViewport(0, 0, 640*2, 480*2);
     
     program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
     
     viewMatrix = glm::mat4(1.0f);
     modelMatrix = glm::mat4(1.0f);
+    marioMatrix = glm::mat4(1.0f);
+    modelMatrix2 = glm::mat4(1.0f);
     projectionMatrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
     
     program.SetProjectionMatrix(projectionMatrix);
     program.SetViewMatrix(viewMatrix);
-    //program.SetColor(1.0f, 0.0f, 0.0f, 1.0f);
     
     glUseProgram(program.programID);
     
@@ -77,6 +83,9 @@ void Initialize() {
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     
     fireballTextureID = LoadTexture("fireball.png");
+    fireballTextureID2 = LoadTexture("fireball.png");
+    mushroomTextureID = LoadTexture("mushroom.png");
+    marioTextureID = LoadTexture("mario.png");
 }
 
 void ProcessInput() {
@@ -95,17 +104,50 @@ void Update() {
     float deltaTime = ticks - lastTicks;
     lastTicks = ticks;
     
-    ball_x += 1.0f * deltaTime;
+    //fireball movement
+    ball_x += 1.4f * deltaTime;
     ball_rotate -= 360.0f * deltaTime;
     
     modelMatrix = glm::mat4(1.0f);
-    modelMatrix = glm::translate(modelMatrix, glm::vec3(ball_x, 0.0f,0.0f));
+    modelMatrix = glm::translate(modelMatrix, glm::vec3(ball_x, -3.0f,0.0f));
     modelMatrix = glm::rotate(modelMatrix, glm::radians(ball_rotate), glm::vec3(0.0f,0.0f,1.0f));
+    
+    modelMatrix2 = glm::mat4(1.0f);
+    modelMatrix2 = glm::translate(modelMatrix2, glm::vec3(ball_x, -2.0f,0.0f));
+    modelMatrix2 = glm::rotate(modelMatrix2, glm::radians(ball_rotate), glm::vec3(0.0f,0.0f,1.0f));
+    
+    //mushroom movement
+    mush_x += 0.55f * deltaTime;
+    modelMatrix3 = glm::mat4(1.0f);
+    modelMatrix3 = glm::translate(modelMatrix3, glm::vec3(mush_x, -3.0f,0.0f));
+    
+    
+    //mario movement
+    if (mario_y > 0){
+        mario_up = false;
+    }else if (mario_y < -3){
+        mario_up = true;
+    }
+    
+    if (mario_up){
+        mario_y += 1.0 * deltaTime;
+        marioMatrix = glm::mat4(1.0);
+        marioMatrix = glm::translate(marioMatrix, glm::vec3(4.0f,mario_y,0.0f));
+    }else{
+        mario_y -= 1.0 * deltaTime;
+        marioMatrix = glm::mat4(1.0);
+        marioMatrix = glm::translate(marioMatrix, glm::vec3(4.0f,mario_y,0.0f));
+    }
+    
+    if (ticks > 16.3){
+        marioMatrix = glm::scale(marioMatrix, glm::vec3(1.5f,1.5f,1.5f));
+    }
 }
 
 void Render() {
     glClear(GL_COLOR_BUFFER_BIT);
     
+    //fireball1
     program.SetModelMatrix(modelMatrix);
     
     float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5};
@@ -117,6 +159,39 @@ void Render() {
     glEnableVertexAttribArray(program.texCoordAttribute);
     
     glBindTexture(GL_TEXTURE_2D, fireballTextureID);
+    glDrawArrays(GL_TRIANGLES,0,6);
+    
+    //fireball2
+    program.SetModelMatrix(modelMatrix2);
+    
+    glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program.positionAttribute);
+    glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program.texCoordAttribute);
+    
+    glBindTexture(GL_TEXTURE_2D, fireballTextureID2);
+    glDrawArrays(GL_TRIANGLES,0,6);
+    
+    //mushroom
+    program.SetModelMatrix(modelMatrix3);
+    
+    glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program.positionAttribute);
+    glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program.texCoordAttribute);
+    
+    glBindTexture(GL_TEXTURE_2D, mushroomTextureID);
+    glDrawArrays(GL_TRIANGLES,0,6);
+    
+    //mario
+    program.SetModelMatrix(marioMatrix);
+    
+    glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program.positionAttribute);
+    glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program.texCoordAttribute);
+    
+    glBindTexture(GL_TEXTURE_2D, marioTextureID);
     glDrawArrays(GL_TRIANGLES,0,6);
     
     glDisableVertexAttribArray(program.positionAttribute);
